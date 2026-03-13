672e7e5e1efffeb5e187e66346761221fb77462e
EfraC26 <efrainbaltodanopiox@gmail.com>
2026-03-13 00:42:08 -0600

    Redireccion paginas

--- reportall/src/app/page.jsx
+++ reportall/src/app/page.jsx
@@ -1,19 +1,5 @@
-import ButtonGroup from './components/ButtonGroup ';
-import MyMap from './components/MyMap';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  return (
-    <>
-      <ButtonGroup
-        containerClass="flex items-center justify-center mt-12 gap-12"
-        buttonClass="w-80 m-auto mt-3"
-        buttons={[
-          { label: "Enacal", href: "/dashboard/enacal" },
-          { label: "Clientes", href: "/dashboard/clientes" },
-        ]}
-      />
-
-      {/* <MyMap autoSelectCurrentLocation /> */}
-    </>
-  );
+  redirect('/auth/login');
 }
